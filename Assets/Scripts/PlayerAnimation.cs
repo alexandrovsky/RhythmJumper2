4ee1201a7d3c525d5f25fc06c5aff271a99007ac
dmitry alexandrovsky <alexandrovsky.dmitry@gmail.com>
2016-03-21 11:00:44 +0100

    tile level seems to work

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	Vector3 lastPos;
+	Animator anim;
+	void Start () {
+		anim = GetComponent<Animator>();
+
+		lastPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float speed = (lastPos - transform.position).magnitude;
+
+		anim.SetFloat("Speed", speed);
+		Debug.Log("Speed" + speed);
+		lastPos = transform.position;
+
+	}
+}
--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -9,6 +9,8 @@ public class PlayerJump : MonoBehaviour {
 	Rigidbody2D rb;
 	JumpVelocityCalculator jumpCalculator;
 
+	bool isJumping, doubleJump;
+
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		levelGenerator = GameObject.Find("LevelGenerator").GetComponent<LevelGenerator>();
@@ -17,21 +19,31 @@ public class PlayerJump : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-
-	}
-
-	void OnCollisionStay2D(Collision2D coll ) // C#, type first, name in second
-	{
-		if (coll.gameObject.layer == LayerMask.NameToLayer("Default") && (Input.GetButtonDown("Jump")))
-		{
-
-//			jumpCalculator.distanceToNextPlatform
-
-			//Jump Script
-//			rb.velocity = Vector2.up * jumpSpeed * levelGenerator.platformHeight;
-
-			float jumpForce = Mathf.Sqrt(2f * Physics2D.gravity.magnitude * rb.gravityScale * jumpHeight) * rb.mass;
-			rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+		if(isJumping && !doubleJump){
+			
 		}
 	}
+
+	void OnCollisionStay2D(Collision2D coll ) {
+		
+		if (coll.gameObject.layer == LayerMask.NameToLayer("Default") ) {
+			if(GetJumpInput()){
+				ApplyJump();
+			}
+
+		}
+	}
+
+	bool GetJumpInput(){
+		return Input.GetButton("Jump") || Input.GetKey(KeyCode.UpArrow);
+	}
+
+	void ApplyJump(){
+		
+		//rb.velocity = Vector2.up * jumpHeight * levelGenerator.platformHeight;
+
+		float jumpForce = Mathf.Sqrt(2f * Physics2D.gravity.magnitude * rb.gravityScale * jumpHeight) * rb.mass;
+		rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+	}
+
 }
